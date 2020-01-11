323e2573bf9ad88c7b8cd57c11e3c6ac11a8088a
Jim Schmid <jim.schmid@1up.io>
2020-01-11 21:02:20 +0100

    Added a few tests for the event object

---
+++ spec/event_spec.lua
@@ -0,0 +1,46 @@
+-- Busted runner
+require "busted.runner"()
+
+-- Modules
+local Dispatcher = require "dispatcher"
+local Event = require "event"
+
+describe("Event", function()
+    it("should be possible to create an event without data", function()
+        local event = Event:new()
+
+        assert.same("table", type(event.data))
+        assert.same(0, #event.data)
+    end)
+
+    it("should be possible to change the data", function()
+        local event = Event:new()
+
+        assert.same("table", type(event.data))
+
+        event.data = 1
+
+        assert.same("number", type(event.data))
+        assert.same(1, event.data)
+    end)
+
+    it("should be possible to change the property isDispatched", function()
+        local event = Event:new()
+
+        assert.is_not.truthy(event.isDispatched)
+
+        event.isDispatched = true
+
+        assert.truthy(event.isDispatched)
+    end)
+
+    it("should be possible to change the property isPropagationStopped", function()
+        local event = Event:new()
+
+        assert.is_not.truthy(event.isPropagationStopped)
+
+        event.isPropagationStopped = true
+
+        assert.truthy(event.isPropagationStopped)
+    end)
+end)
